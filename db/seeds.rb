928023ccb8226668e5d12b88b6e14c2ce991cc9f
Sy Rashid <sy.rashid.11@gmail.com>
2020-10-20 23:49:19 +0300

    FEAT: Set up mapbox and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+sy = Flat.create(name:"Sy Rashid", address: "183 Bridge Rd, Melbourne")
+sy.update(name: "Sy Radish")
+sy.update(address: "Kurdela Sk 11, Beyoğlu/Istanbul")
+
+ed = Flat.create(name:"Ed", address: "Hacımimi, Dibek Sk. 15-2, 34425 Beyoğlu/İstanbul")
